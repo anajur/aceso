e8ae12f0f3d8b3bcf10ccce35f367d363b6e6d92
Ana Júlia da Rosa <56845067+anajur@users.noreply.github.com>
2026-05-31 19:18:35 -0300

    criando estrutura api

---
+++ api/aceso/src/main/java/com/aceso/aceso/AcesoApplication.java
@@ -0,0 +1,13 @@
+package com.aceso.aceso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcesoApplication.class, args);
+	}
+
+}
---
+++ api/aceso/src/test/java/com/aceso/aceso/AcesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceso.aceso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
